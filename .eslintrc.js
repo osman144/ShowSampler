b2bc837462c88d3e66ba924aaf86132d83183915
osman144 <osman144@umn.edu>
2018-03-31 11:19:24 -0500

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = { 
+    'extends': 'airbnb-base',
+    'rules': {
+      'no-plusplus': [0],
+      'strict': [0],
+      'space-before-function-paren': [0],
+      'func-names': [0],
+      'object-shorthand': [0],
+      'prefer-arrow-callback': [0]
+    } 
+  };
